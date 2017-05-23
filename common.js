5b093a73cd2f2858d63e925e49a83f633f630eec
LooySa <13580523733@163.com>
2017-05-23 10:28:32 +0800

    common
    common js that contains common function

---
+++ common.js
@@ -0,0 +1,7 @@
+  common.getUrlParam = function(key) {
+        var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return decodeURI(r[2]);
+        return null;
+    };
+  return common;
